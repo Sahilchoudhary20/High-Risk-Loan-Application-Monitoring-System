002d0d94042db652fa98617eb84ff9c3be3c355e
Sahilchoudhary20 <choudharysahil4309@gmail.com>
2025-11-10 02:06:51 +0530

    updated some more details

--- test/jest.config.js
+++ test/jest.config.js
@@ -1,7 +1,20 @@
 module.exports = {
-  preset: 'ts-jest',
+  preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'node',
   roots: ['<rootDir>/test'],
+  transform: {
+    '^.+\\.(t|j)s$': ['ts-jest', { useESM: true }]
+  },
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1'
+  },
+  globals: {
+    'ts-jest': {
+      useESM: true,
+      tsconfig: 'tsconfig.json'
+    }
+  },
   collectCoverage: true,
   coverageDirectory: 'coverage'
 };
